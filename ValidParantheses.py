8d2c3fbfeb1cf28ba6f63ccdfcf15191a5c962e1
Arjunn Shetty <131944608+PhantomCreaper@users.noreply.github.com>
2024-04-03 16:48:16 +0530

    Create ValidParantheses.py

---
+++ ValidParantheses.py
@@ -0,0 +1,13 @@
+#Main not included
+class Solution:
+    def isValid(self, s):
+        stack = []
+        bracket_mapping = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in bracket_mapping.values():
+                stack.append(char)
+            elif char in bracket_mapping.keys():
+                if not stack or stack.pop() != bracket_mapping[char]:
+                    return False
+        return len(stack)==0
