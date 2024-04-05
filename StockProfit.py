20ca9e6cf005e5066d35949f450bafdbb54eaea7
Arjunn Shetty <131944608+PhantomCreaper@users.noreply.github.com>
2024-04-05 20:57:07 +0530

    Create StockProfit.py

---
+++ StockProfit.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        l, r = 0, 1
+        Pmax = 0
+        while(r<len(prices)):
+            if prices[l]<prices[r]:
+                p = prices[r]-prices[l]
+                Pmax = max(Pmax, p)
+            else:
+                l = r
+            r+=1
+           
+        return Pmax
