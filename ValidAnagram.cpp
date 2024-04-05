0e365cb3e7de0270337d90402de0f2c4b50ed228
Arjunn Shetty <131944608+PhantomCreaper@users.noreply.github.com>
2024-04-05 22:36:53 +0530

    Create ValidAnagram.cpp

---
+++ ValidAnagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> count;
+        if(s.length()!=t.length())
+            return false;
+        for(char c : s){
+            count[c]++;
+        }
+        for(char c : t){
+            if(--count[c]<0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
