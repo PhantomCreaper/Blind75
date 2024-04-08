8ce3829414b2a554d9e8c8cdf6a6e12a3178cdf9
Arjunn Shetty <131944608+PhantomCreaper@users.noreply.github.com>
2024-04-08 22:11:20 +0530

    Create RPN.py

---
+++ RPN.py
@@ -0,0 +1,22 @@
+# 150. Reverse Polish Notation
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack=[]
+        for c in tokens:
+            if c=='+':
+                stack.append(stack.pop()+stack.pop())
+            elif c=='-':
+                a,b=stack.pop(),stack.pop()
+                stack.append(b-a)
+            elif c=='*':
+                stack.append(stack.pop()*stack.pop())
+            elif c=='/':
+                a,b=stack.pop(),stack.pop()
+                stack.append(int(float(b)/a))
+            else:
+                stack.append(int(c))
+        return stack[-1]
